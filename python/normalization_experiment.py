bd26baf0f7bd6d83b587477e92a7a86b46929b96
Dabrze <dariusz.brzezinski@cs.put.poznan.pl>
2018-09-18 21:22:15 +0200

    Updated normalization experiment, including new datasets

--- python/normalization_experiment.py
+++ python/normalization_experiment.py
@@ -16,16 +16,12 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import cross_val_score, RepeatedStratifiedKFold
+from sklearn.model_selection import cross_validate, RepeatedStratifiedKFold
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
 SEED = 23
 DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
-breast_X = os.path.join(DATA_PATH, "breast-w_X.csv")
-breast_y = os.path.join(DATA_PATH, "breast-w_y.csv")
-thyroid_X = os.path.join(DATA_PATH, "new-thyroid_X.csv")
-thyroid_y = os.path.join(DATA_PATH, "new-thyroid_y.csv")
 
 classifiers = {
     "kNN": KNeighborsClassifier(),
@@ -37,84 +33,93 @@ classifiers = {
     "GBM": GradientBoostingClassifier(random_state=SEED),
 }
 scorers = {
+    "Accuracy": utils.accuracy,
+    "Bal. acc.": utils.balanced_accuracy,
+    "Recall": utils.recall,
+    "Precision": utils.precision,
+    "F1-score": utils.f1_score,
+    "G-mean": utils.g_mean,
+    "Kappa": utils.kappa,
+    "MCC": utils.mcc,
+}
+scikit_scorers = {
     "Accuracy": get_scorer("accuracy"),
-    "Balanced accuracy": get_scorer("recall_macro"),
+    "Bal. acc.": get_scorer("recall_macro"),
     "Recall": get_scorer("recall"),
     "Precision": get_scorer("precision"),
     "F1-score": get_scorer("f1"),
-    "G-mean": make_scorer(utils.g_mean),
+    "G-mean": make_scorer(utils.g_mean_scikit),
     "Kappa": make_scorer(cohen_kappa_score),
     "MCC": make_scorer(matthews_corrcoef),
 }
 datasets = {
-    "transfusion": (pd.read_csv(os.path.join(DATA_PATH, "transfusion_X.csv"),
-                                index_col=False),
-                    pd.read_csv(os.path.join(DATA_PATH, "transfusion_y.csv"),
-                                index_col=False)),
-    "glass": (pd.read_csv(os.path.join(DATA_PATH, "glass_X.csv"),
-                          index_col=False),
-              pd.read_csv(os.path.join(DATA_PATH, "glass_y.csv"),
-                          index_col=False)),
-    "breast-w": (pd.read_csv(os.path.join(DATA_PATH, "breast-w_X.csv"),
-                             index_col=False),
-                 pd.read_csv(os.path.join(DATA_PATH, "breast-w_y.csv"),
-                             index_col=False)),
-    "ionosphere": (pd.read_csv(os.path.join(DATA_PATH, "ionosphere_X.csv"),
-                               index_col=False),
-                   pd.read_csv(os.path.join(DATA_PATH, "ionosphere_y.csv"),
-                               index_col=False)),
-    "new-thyroid": (pd.read_csv(os.path.join(DATA_PATH, "new-thyroid_X.csv"),
-                                index_col=False),
-                    pd.read_csv(os.path.join(DATA_PATH, "new-thyroid_y.csv"),
-                                index_col=False)),
-    "balance-scale": (pd.read_csv(os.path.join(DATA_PATH, "balance-scale_X.csv"),
-                          index_col=False),
-              pd.read_csv(os.path.join(DATA_PATH, "balance-scale_y.csv"),
-                          index_col=False)),
+    "transfusion": "yes", "glass": "v-float", "breast-w": "malignant",
+    "ionosphere": "bad", "new-thyroid": "hyper", "arcene": "positive",
+    "colon": "1", "micromass": "AUG.AEX", "yeast": "ME2", "solar-flare": "F",
+    "ecoli": "imU", "credit-g": "bad"
 }
+
 
 if __name__ == '__main__':
     i = 0
     result_df = pd.DataFrame()
+    dataset_df = pd.DataFrame()
     hists = dict()
 
-    for d in datasets:
-        X = datasets[d][0]
-        y = datasets[d][1]
+    for d_idx, d in enumerate(datasets.keys()):
+        print(str(d))
+        X = pd.read_csv(os.path.join(DATA_PATH, d + "_X.csv"), index_col=False)
+        y = pd.read_csv(os.path.join(DATA_PATH, d + "_y.csv"), index_col=False)
 
-        print(str(d) + ": Calculating histograms for all scorers...")
+        dataset = {}
+        dataset["Name"] = d
+        dataset["Examples"] = X.shape[0]
+        dataset["Features"] = X.shape[1]
+        dataset["Imbalance ratio"] = float(y.iloc[:, 0].value_counts()[1])/float(y.shape[0])
+        dataset["Minority class"] = datasets[d]
+
+        if dataset_df is None:
+            dataset_df = pd.DataFrame(dataset, index=[d_idx])
+        else:
+            dataset_df = dataset_df.append(pd.DataFrame(dataset, index=[d_idx]))
+
+        print(str(d) + ": Calculating pmf for all scorers...")
         for s in scorers:
             hists[s] = utils.calculate_hist(scorers[s], y)
 
         for c in classifiers:
-            result = {"Accuracy": 0, "Balanced accuracy": 0, "Recall": 0,
-                      "Precision": 0, "F1-score": 0, "G-mean": 0, "Kappa": 0,
-                      "MCC": 0, "Classifier": c, "Dataset": d,
-                      "Accuracy norm": 0, "Balanced accuracy norm": 0,
-                      "Recall norm": 0, "Precision norm": 0,
-                      "F1-score norm": 0, "G-mean norm": 0, "Kappa norm": 0,
-                      "MCC norm": 0}
+            print(str(d) + ": " + str(c) + "... ")
+            clf = make_pipeline(StandardScaler(), clone(classifiers[c]))
 
-            for s in scorers:
-                print(str(d) + ": " + str(c) + ": " + str(s) + "... ")
-                clf = make_pipeline(StandardScaler(), clone(classifiers[c]))
-                r = cross_val_score(clf, X, y, scoring=scorers[s],
-                                    cv=RepeatedStratifiedKFold(10, 10, SEED))
-                result[s] = np.mean(r)
-                result[s + " norm"] = np.mean(utils.hnorm(r, hists[s]))
+            r = cross_validate(clf, X, y, scoring=scikit_scorers,
+                               cv=RepeatedStratifiedKFold(10, 10, SEED),
+                               return_train_score=False)
 
-            if result_df is None:
-                result_df = pd.DataFrame(result, index=[i])
-            else:
+            for s in scikit_scorers:
+                result = {"Id": 0, "Dataset": d, "Classifier": c,
+                          "Measure": None, "Type": None, "Mean": 0, "Std": 0}
+                result["Measure"] = s
+                result["Mean"] = np.mean(r["test_" + s])
+                result["Std"] = np.std(r["test_" + s])
+                result["Type"] = "Standard"
+                result["Id"] = i
+                if result_df is None:
+                    result_df = pd.DataFrame(result, index=[i])
+                else:
+                    result_df = result_df.append(pd.DataFrame(result, index=[i]))
+
+                result["Measure"] = s
+                result["Mean"] = np.mean(utils.hnorm(r["test_" + s], hists[s]))
+                result["Std"] = np.std(utils.hnorm(r["test_" + s], hists[s]))
+                result["Type"] = "Normalized"
+                result["Id"] = i + 1
                 result_df = result_df.append(pd.DataFrame(result, index=[i]))
-            i = i + 1
+            i = i + 2
 
-    result_df = result_df[["Dataset", "Classifier", "Accuracy",
-                           "Balanced accuracy", "Kappa", "G-mean", "F1-score",
-                           "Precision", "Recall", "MCC", "Accuracy norm",
-                           "Balanced accuracy norm", "Kappa norm",
-                           "G-mean norm", "F1-score norm", "Precision norm",
-                           "Recall norm", "MCC norm"]]
+    result_df = result_df[["Id", "Dataset", "Classifier", "Measure",
+                           "Type", "Mean", "Std"]]
     result_df.to_csv("ExperimentResults.csv", index=False)
 
-
+    dataset_df = dataset_df[["Name", "Examples", "Features", "Imbalance ratio",
+                             "Minority class"]]
+    dataset_df.to_csv("Datasets.csv", index=False)
--- python/utils.py
+++ python/utils.py
@@ -10,7 +10,7 @@ from sklearn.metrics.classification import _prf_divide
 from sklearn.preprocessing import LabelEncoder
 
 
-def g_mean(y_true, y_pred, labels=None, correction=0.001):
+def g_mean_scikit(y_true, y_pred, labels=None, correction=0.0):
     present_labels = unique_labels(y_true, y_pred)
 
     if labels is None:
@@ -50,10 +50,10 @@ def g_mean(y_true, y_pred, labels=None, correction=0.001):
     return sp.stats.mstats.gmean(recall)
 
 
-def calculate_hist(scorer, y):
+def calculate_hist_slow(scorer, y):
     # very general implementation, but very slow - can be easily vectorized if
     # one implements scorers as simple vector functions instead of what they
-    # are in scikit
+    # are in scikit (as done below)
     hist = []
     n = y.values.__len__() # all examples
     P = np.sum(y.values == 1) # positives
@@ -73,6 +73,77 @@ def calculate_hist(scorer, y):
         hist.append(scorer._score_func(y_true, y_pred))
 
     return np.array(hist)
+
+
+def accuracy(cm):
+    return (cm[:, 0] + cm[:, 3]).astype(np.float) / (cm[:, 0] + cm[:, 1] + cm[:, 2] + cm[:, 3]).astype(np.float)
+
+
+def balanced_accuracy(cm):
+    return np.nan_to_num((cm[:, 0]).astype(np.float) / (cm[:, 0] + cm[:, 1]).astype(np.float) +
+            (cm[:, 3]).astype(np.float) / (cm[:, 2] + cm[:, 3]).astype(np.float)) / 2.0
+
+
+def recall(cm):
+    return np.nan_to_num(cm[:, 0]).astype(np.float) / (cm[:, 0] + cm[:, 1]).astype(np.float)
+
+
+def precision(cm):
+    return np.nan_to_num((cm[:, 0]).astype(np.float) / (cm[:, 0] + cm[:, 2]).astype(np.float))
+
+
+def f1_score(cm):
+    return np.nan_to_num(2.0 * (precision(cm) * recall(cm)) / (precision(cm) + recall(cm)))
+
+
+def g_mean(cm):
+    return np.nan_to_num(
+        np.sqrt(
+            (cm[:, 0]).astype(np.float) / (cm[:, 0] + cm[:, 1]).astype(np.float) *
+            (cm[:, 3]).astype(np.float) / (cm[:, 2] + cm[:, 3]).astype(np.float)
+        )
+    )
+
+
+def kappa(cm):
+    a = (cm[:, 0]).astype(np.float)
+    b = (cm[:, 2]).astype(np.float)
+    c = (cm[:, 1]).astype(np.float)
+    d = (cm[:, 3]).astype(np.float)
+
+    n = a + b + c + d
+    N = b + d
+    P = a + c
+    P_ = a + b
+    N_ = c + d
+
+    return np.nan_to_num(
+        (accuracy(cm) - 1 / n * (P * P_ / n + N * N_ / n)) /
+        (1 - 1 / n * (P * P_ / n + N * N_ / n))
+    )
+
+
+def mcc(cm):
+    a = (cm[:, 0]).astype(np.float)
+    b = (cm[:, 2]).astype(np.float)
+    c = (cm[:, 1]).astype(np.float)
+    d = (cm[:, 3]).astype(np.float)
+    return np.nan_to_num((a * d - b * c) / np.sqrt(((a + b) * (b + d) * (a + c) * (c + d))))
+
+
+def calculate_hist(scorer, y):
+    n = y.values.__len__() # all examples
+    P = np.sum(y.values == 1) # positives
+    N = n - P # negatives
+
+    t = np.concatenate((np.transpose(
+        np.kron([np.arange(0, P + 1, 1), np.arange(P, -1, -1)],
+                np.ones(N + 1))),
+        np.tile(np.transpose([np.arange(0, N + 1, 1), np.arange(N, -1, -1)]),
+                (P + 1, 1))),
+        axis=1).astype(int)
+
+    return scorer(t)
 
 
 def hnorm(values, hist):
